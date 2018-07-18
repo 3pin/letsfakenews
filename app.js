354fd43c348df7e6826c7dbb94e44d20609a3e49
3pin <3pin.audiovisual@gmail.com>
2018-07-18 11:23:53 +0100

    follow up on previous commit

--- app.js
+++ app.js
@@ -37,19 +37,6 @@ var digest = auth.digest({
   file: __dirname + "/htpasswd",
   authType: "digest"
 });
-//use authorization
-app.use(function(req, res, next) {
-  if ('/display' === req.path) {
-    (auth.connect(digest))(req, res, next)
-  } else if ('/display/' === req.path) {
-    (auth.connect(digest))(req, res, next)
-  } else {
-    next()
-  }
-})
-/*
-app.use(auth.connect(digest))
-*/
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -69,14 +56,30 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
 //app.use('/scripts', express.static(__dirname + '/node_modules/'));
 
+//use authorization
+app.use(function(req, res, next) {
+  if ('/' === req.path) {
+    next()
+  } else if ('/display' === req.path) {
+    (auth.connect(digest))(req, res, next)
+  } else if ('/display/' === req.path) {
+    (auth.connect(digest))(req, res, next)
+  } else {
+    next()
+  }
+})
+/*
+app.use(auth.connect(digest))
+*/
+
+// define routes
+app.use('/', index);
+
 // Make our db accessible to our router
 app.use(function(req, res, next) {
   req.db = db;
   next();
 });
-
-// define routes
-app.use('/', index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
