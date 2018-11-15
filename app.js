d76eb7f74eff0c29215d37d9cab9a1ccfecef0ec
3pin <3pin.audiovisual@gmail.com>
2018-11-15 13:44:26 +0000

    created and setup favicon

--- app.js
+++ app.js
@@ -40,9 +40,9 @@ app.use(auth.connect(digest))
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+// uncomment after placing your favicon in /public
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false
