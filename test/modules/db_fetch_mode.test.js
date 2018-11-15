427de73f90f8f26ca831a65ba617a3278fceae9a
3pin <3pin.audiovisual@gmail.com>
2018-11-15 13:21:15 +0000

    added tests for db_fetch_mode

---
+++ test/modules/db_fetch_mode.test.js
@@ -0,0 +1,23 @@
+// load the ENVIRONMENT variables
+require('dotenv').config();
+const debug = require('debug')('tests')
+
+// load assertion library
+const chai = require('chai'),
+  expect = chai.expect,
+  should = chai.should();
+
+const db_fetch_mode = require('../../modules/db_fetch_mode.js');
+
+describe('testing functions controlling fetch-mode of stories from the db...', () => {
+  it('should take an array, select a random entry in array, return its value with key:id in a jsonOBJ...', () => {
+    let result = db_fetch_mode.random_entry(['a','b','c'])
+    debug(result)
+    expect(result.id).to.be.a('string')
+  })
+  it('should take an array and an entry-to-pick that is <= array.length... return its value with key:id in a jsonOBJ...', () => {
+    let result = db_fetch_mode.next_entry(['a','b','c'], 3)
+    debug(result)
+    expect(result.id).to.equal('a')
+  })
+});
--- test/modules/process_client_module.test.js
+++ test/modules/process_client_module.test.js
@@ -1,20 +1,13 @@
-// check the env
-if (process.env.NODE_ENV !== 'production') {
-  const path = require('path');
-  const dotEnvPath = path.resolve('./.env');
-  const dotenv = require('dotenv')
-  const result = dotenv.config({ path: dotEnvPath})
-  if (result.error) {
-    throw result.parsed
-  }
-}
+// load the ENVIRONMENT variables
+require('dotenv').config();
+const debug = require('debug')('tests')
 
 // load assertion library
 const chai = require('chai'),
   expect = chai.expect,
   should = chai.should();
 
-const process_client_module = require('../modules/process_client_module.js');
+const process_client_module = require('../../modules/process_client_module.js');
 
 describe('When story(JSON) recevied... story-processed to [words] to [urls] then JSON', function() {
   it('from client-JSON parse nouns -> fetch urls -> calc currenttime -> add client-JSON', (done) => {
