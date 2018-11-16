cb6ee89f98a2453f9e9a968af705b3a246c4cbe9
3pin <3pin.audiovisual@gmail.com>
2018-11-16 12:41:09 +0000

    Added strict mode to modules ASync'd all modules execpt the db_fetch_modes which dont need it Fixed the hanging of the process_client_module test by cleaning up promises Added tests for the time_ops module

--- test/modules/nlp_module.test.js
+++ test/modules/nlp_module.test.js
@@ -9,7 +9,7 @@ const chai = require('chai'),
 
 const nlp_module = require('../../modules/nlp_module.js');
 
-describe('When a new fakenewsreport (story) is received...', function() {
+describe('When a new fakenewsreport (story) is received...', () => {
 
   it('should NLP the story(string) to an array of nouns...', (done) => {
     nlp_module.parse_nouns('The cat ran under the bus').then(result => {
@@ -18,7 +18,7 @@ describe('When a new fakenewsreport (story) is received...', function() {
   })
 
   it('should NLP the story(string) to an array of nouns PER phrase...', (done) => {
-    nlp_module.parse_phrases('The cat ran under the bus. It was hurt').then(result => {
+    nlp_module.parse_phrases('The cat ran under the bus. It was hurt').then((result) => {
       expect(result).to.be.an('array');
     }).finally(done);
   })
