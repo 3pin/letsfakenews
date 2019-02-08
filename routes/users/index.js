bdf7d11a0c4ff19886d7114b901157c2945b1357
3pin <3pin.audiovisual@gmail.com>
2019-02-08 22:00:29 +0000

    Moved main.ejs frontend file into a 'users' folder

--- routes/users/index.js
+++ routes/users/index.js
@@ -4,25 +4,13 @@
 const routes = require('express').Router();
 
 /* routes */
-const landing = require('./main');
-//const sse = require('./sse');
-//const mode = require('./mode');
+const users = require('./main');
 const add_title_story = require('./add_title_story');
 const add_feedback = require('./add_feedback');
-/* other routes */
-//const databases = require('./databases');
-//const displays = require('./displays');
 
 /* this router */
-routes.get('/', landing);
-//routes.get('/sse', sse);
-//routes.get('/mode', mode);
+routes.get('/', users);
 routes.post('/add_title_story', add_title_story);
 routes.post('/add_feedback', add_feedback);
-
-/* other routers
-routes.use('/databases', databases);
-routes.use('/displays', displays);
-*/
 
 module.exports = routes;
--- routes/users/main.js
+++ routes/users/main.js
@@ -5,7 +5,7 @@ const debug = require('debug')('users/main')
 module.exports = (req, res) => {
   // serve homepage
   debug('/GET routes/main')
-  res.render('main', {
+  res.render('users/main', {
     tabtitle: "LetsFakeNews:Users"
   });
 }
