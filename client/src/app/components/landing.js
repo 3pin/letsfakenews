364dc5aadc6499d9740e6a389e923b7eb70d152f
3pin <3pin.audiovisual@gmail.com>
2019-02-22 18:43:48 +0000

    Added autoFocus to textarea title input

--- client/src/app/components/landing.js
+++ client/src/app/components/landing.js
@@ -3,7 +3,6 @@ import {Link} from 'react-router-dom';
 
 export default class Landing extends React.Component {
   render() {
-    console.log(this.props)
     
     return (<div>
       <section className="section" id="1">
