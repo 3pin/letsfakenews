97c8f98be3d32da49541fd6d89902daa245dc646
3pin <3pin.audiovisual@gmail.com>
2019-04-01 23:09:19 +0100

    Removed refs to app.locals across remaining routes

--- controllers/settings/mode.js
+++ controllers/settings/mode.js
@@ -6,11 +6,7 @@ module.exports = (req, res) => {
   // serve mode-data to client
   debug('/GET routes/mode');
   let dbSettings = req.dbSettings;
-  let mode = process.env.NODE_ENV;
-  let autolive = req.app.locals.autolive;
   res.send({
-    mode: mode,
-    autolive: autolive,
     dbSettings: dbSettings
   });
 }
--- controllers/watch/request_new_story.js
+++ controllers/watch/request_new_story.js
@@ -10,24 +10,22 @@ let id;
 module.exports = (req, res) => {
   // serve story to displaypage when-previous-story-finished
   debug('/GET routes/displays/request_new_story')
-
+  let dbSettings = req.dbSettings;
   // choose an id from activelist[]...
-  debug(req.app.locals.activelist);
   let db_fetch_mode = require('../../modules/db_fetch_mode.js');
   let obj;
-  if (req.app.locals.db_mode == 'next') {
-    obj = db_fetch_mode.next_entry(req.app.locals.activelist, req.app.locals.entry_to_read);
+  if (dbSettings.db_mode == 'next') {
+    obj = db_fetch_mode.next_entry(dbSettings.activelist, dbSettings.entry_to_read);
     // set global vars for next-sequential-time-around
-    req.app.locals.entry_to_read = obj.entry_to_read;
+    dbSettings.entry_to_read = obj.entry_to_read;
     debug(obj.entry_to_read);
   } else {
-    obj = db_fetch_mode.random_entry(req.app.locals.activelist);
+    obj = db_fetch_mode.random_entry(dbSettings.activelist);
   }
-  req.app.locals.db_mode = obj.db_mode;
-  debug(req.app.locals.db_mode);
+  dbSettings.db_mode = obj.db_mode;
+  debug(dbSettings.db_mode);
   id = obj.id;
   debug('id to read from activelist: ' + id);
-
   // fetch the JSON from db
   debug('fetching the obj from db');
   Story.findById(id, (err, data) => {
