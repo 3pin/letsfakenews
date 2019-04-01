a2895507e44b5f292164b960bff79e4df2f27194
3pin <3pin.audiovisual@gmail.com>
2019-04-01 21:02:18 +0100

    Added console.log to check why root is not letting pages be refreshed

--- app.js
+++ app.js
@@ -65,6 +65,8 @@ app.use(cookieParser());
 // Serve static files from the React app
 // ... production mode
 if (process.env.NODE_ENV === 'production') {
+  console.log('mode: production');
+  console.log(`${__dirname}client/build`);
   app.use(express.static(path.join(__dirname, 'client/build')));
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname = 'client/build/index.html'));
--- routes/settings.js
+++ routes/settings.js
@@ -11,7 +11,7 @@ const activelist = require('../controllers/settings/activelist');
 
 const cors = require('cors');
 // Set up a whitelist and check against it:
-const whitelist = [`http://localhost:${process.env.REACT_PORT}`, `${process.env.FRONTEND_URL}/admin/stories`, `${process.env.FRONTEND_URL}/admin/feedback`]
+const whitelist = [`http://localhost:${process.env.REACT_PORT}`, 'https://letsfakenews.herokuapp.com/', `https://letsfakenews.herokuapp.com/admin/stories`]
 const corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
@@ -23,7 +23,7 @@ const corsOptions = {
 }
 
 /* this router */
-routes.get('/sse', cors(), sse);
+routes.get('/sse', cors(corsOptions), sse);
 routes.get('/mode', mode);
 routes.get('/activelist', activelist);
 
