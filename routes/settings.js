0950ee9980443ab6a6ddb3cffd0b881f348f1447
3pin <3pin.audiovisual@gmail.com>
2019-04-01 20:43:20 +0100

    Using cors() without whitelist included in options

--- routes/settings.js
+++ routes/settings.js
@@ -11,7 +11,7 @@ const activelist = require('../controllers/settings/activelist');
 
 const cors = require('cors');
 // Set up a whitelist and check against it:
-const whitelist = ['http://localhost:3000', 'http://letsfakenews.herokuapp.com']
+const whitelist = [`http://localhost:${process.env.REACT_PORT}`, `${process.env.FRONTEND_URL}/admin/stories`, `${process.env.FRONTEND_URL}/admin/feedback`]
 const corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
@@ -23,7 +23,7 @@ const corsOptions = {
 }
 
 /* this router */
-routes.get('/sse', cors(corsOptions), sse);
+routes.get('/sse', cors(), sse);
 routes.get('/mode', mode);
 routes.get('/activelist', activelist);
 
