47e65333c23171b8f82e06a59b1cae3d74747d32
3pin <3pin.audiovisual@gmail.com>
2019-04-02 11:55:36 +0100

    replaced '/*' => '/'

--- app.js
+++ app.js
@@ -67,8 +67,8 @@ app.use(cookieParser());
 if (process.env.NODE_ENV === 'production') {
   console.log('Serving: ' + path.join(__dirname, '/client/build/index.html'));
   app.use(express.static(path.join(__dirname, '/client/build')));
-  app.get('/*', (req, res) => {
-    res.sendFile(__dirname + '/client/build/index.html');
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/build/index.html'));
   })
 }
 // ... local mode
