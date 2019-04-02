d16a22644c422fb728be76ed205e44a9f7f0c7b2
3pin <3pin.audiovisual@gmail.com>
2019-04-02 11:00:11 +0100

    Build the client/build folder. Removed the heroku-postbuild 'build' script

---
+++ client/build/precache-manifest.158a04f3574773bfdfef277c4105903b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0333dd7b1e1ad332b3fcabf41d80ca4a",
+    "url": "/static/media/bgd.0333dd7b.jpg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "65f479b77fac1684ec44",
+    "url": "/static/js/main.5c1844fa.chunk.js"
+  },
+  {
+    "revision": "4315b9d2671da034e6e2",
+    "url": "/static/js/2.ad865187.chunk.js"
+  },
+  {
+    "revision": "65f479b77fac1684ec44",
+    "url": "/static/css/main.bc428018.chunk.css"
+  },
+  {
+    "revision": "15c355474ba478894e8098d55c70c253",
+    "url": "/index.html"
+  }
+];
