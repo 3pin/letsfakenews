c26261c9c8ca1b36a809195008eb9b5df57a60c1
3pin <3pin.audiovisual@gmail.com>
2019-04-06 18:06:56 +0100

    Setup hamburger to close on click of one of its menu items

--- client/src/app/components/navframe.js
+++ client/src/app/components/navframe.js
@@ -4,19 +4,35 @@ import {Navbar, Nav} from 'react-bootstrap';
 import {LinkContainer} from "react-router-bootstrap";
 //
 export default class IndexLanding extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      navExpanded: false
+    };
+  }
+  getInitialState () {
+    return {
+      navExpanded: false
+    }
+  }
+  setNavExpanded(expanded) {
+    this.setState({ navExpanded: expanded });
+  }
+  closeNav() {
+    this.setState({ navExpanded: false });
+  }
   render() {
     //console.log(this.props);
     const links = this.props.links;
     const linksFrame = links.map((entry, i) => <LinkContainer activeClassName="active" key={i} to={`/${entry}`}><Nav.Link>{entry}</Nav.Link></LinkContainer>);
-
     return (<div>
-      <Navbar bg="light" variant="light" expand="md">
+      <Navbar bg="light" variant="light" expand="md" onToggle={this.setNavExpanded.bind(this)} expanded={this.state.navExpanded}>
         <LinkContainer to={this.props.title}>
           <a className="navbar-brand" href="/">{this.props.title}</a>
         </LinkContainer>
         <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto">
+          <Nav className="ml-auto" onSelect={this.closeNav.bind(this)}>
             {linksFrame}
           </Nav>
         </Navbar.Collapse>
