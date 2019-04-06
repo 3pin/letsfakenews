53bdad6b8404cbea935e40ae154cab51321b37f0
3pin <3pin.audiovisual@gmail.com>
2019-04-06 17:59:09 +0100

    Changed className of smallest bootstrap left-col from col-xs-1 to col-1 and so now responds correctly at smallest screen-widths

--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,7 @@
+//import '../node_modules/bootstrap/dist/js/bootstrap.min.js'
+//import '../node_modules/bootstrap-without-jquery/dist/bootstrap-without-jquery.min.js'
+//import '../node_modules/react-bootstrap/dist/react-bootstrap.min.js';
+//import './maximum.css';
 import './minimum.css';
 
 import React from 'react';
