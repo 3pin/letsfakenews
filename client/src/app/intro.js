7c600a7427a74998ead44eaa2d340e5b91ac0e88
3pin <3pin.audiovisual@gmail.com>
2019-07-30 18:15:01 +0100

    Added this.ref for video in react-player in intro.js

--- client/src/app/intro.js
+++ client/src/app/intro.js
@@ -15,14 +15,15 @@ class Intro extends React.Component {
     return (<div>
       <div id="introContainer">
         <ReactPlayer
-          url='https://res.cloudinary.com/hi58qepi6/video/upload/v1564406783/TEI_Beers.mov'
+          url='https://res.cloudinary.com/hi58qepi6/video/upload/v1564506559/Screengrab_Video_Snipped_Web-desktop.mp4'
           className='introContent react-player'
           id="introVideoPlayer"
           autoPlay={true}
           playing={true}
-          volume={0}
+          volume={1}
           width='100%'
           height='100%'
+          ref={player => { this.player=player }}
           onEnded={this.onEnded}
         />
       <div>
