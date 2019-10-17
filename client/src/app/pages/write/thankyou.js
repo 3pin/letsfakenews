535f816ea4ab530fc81245fc5db557df401557f7
3pin <3pin.audiovisual@gmail.com>
2019-10-17 10:19:08 +0100

    Removed 'watch' option from 'thankyou' page in 'write' route

--- client/src/app/pages/write/thankyou.js
+++ client/src/app/pages/write/thankyou.js
@@ -8,8 +8,6 @@ const Thankyou = () => {
       <section>
         <FrameBanner title="Thank you..." desc="Thank you for your fake news story"/>
         <hr/>
-        <FrameButton buttonLabel="Watch" linkto="/watch" desc="Watch fake news (desktop only)"/>
-        <hr/>
         <FrameButton buttonLabel="Write" linkto="/write/story" desc="Write more fake news"/>
         <hr/>
         <FrameButton buttonLabel="Feedback" linkto="/write/feedback" desc="Leave feedback for the service"/>
