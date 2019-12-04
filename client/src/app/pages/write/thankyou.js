5949818592d7cdeacb7ce98499ca5221aba582c0
3pin <3pin.audiovisual@gmail.com>
2019-12-04 14:10:08 +0000

    There are now 3 buttons on thankyou.js frontend... write / home / feedback

--- client/src/app/pages/write/thankyou.js
+++ client/src/app/pages/write/thankyou.js
@@ -8,7 +8,9 @@ const Thankyou = () => {
       <section>
         <FrameBanner title="Thank you..." desc="Thank you for your fake news story"/>
         <hr/>
-        <FrameButton buttonLabel="Home" linkto="/landing" desc="Return to homepage"/>
+        <FrameButton buttonLabel="Write" linkto="/write/story" desc="Write more fakenews"/>
+        <hr/>
+        <FrameButton variant='secondary' buttonLabel="Home" linkto="/landing" desc="Return to homepage"/>
         <hr/>
         <FrameButton variant='secondary' buttonLabel="Feedback" linkto="/write/feedback" desc="Leave your feedback"/>
         <hr/>
