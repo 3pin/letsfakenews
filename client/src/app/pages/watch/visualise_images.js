01ca5bbb65c70ff0822f65a0387e8ded72696018
3pin <3pin.audiovisual@gmail.com>
2020-01-27 17:13:08 +0000

    Using 'react-crossfade-image' library as an <img/> component that crossfades when 'src' is changed... next: assess why the image scales-down during crossfade

--- client/src/app/pages/watch/visualise_images.js
+++ client/src/app/pages/watch/visualise_images.js
@@ -1,11 +1,14 @@
 import React from "react";
+import CrossfadeImage from "react-crossfade-image";
 import FrameButton from "../../../app/components/frameButton";
 //import Sketch from "./sketches/sketch4class";
 import 'eventsource-polyfill';
+var timerId;
 
 export default class Visualise extends React.Component {
   constructor(props) {
     super(props);
+    this._isMounted = false;
     if (process.env.NODE_ENV === 'production') {
       this.eventSource = new EventSource('/settings/sse');
     } else {
@@ -16,11 +19,19 @@ export default class Visualise extends React.Component {
     this.refreshList = this.refreshList.bind(this);
     this.pickImages = this.pickImages.bind(this);
     this.changeImage = this.changeImage.bind(this);
+    this.startTimer = this.startTimer.bind(this);
+    this.endTimer = this.endTimer.bind(this);
     this.state = {
       apiHello: "/watch/visualise",
       liveList: [],
       imageSet: ["../../images/bgd.jpg"],
-      imageIndex: 0
+      imageIndex: 0,
+      style: {
+        position: 'relative',
+        height: '100%',
+        width: '100%',
+        objectFit: 'cover',
+      }
     };
   }
   apiGet = async endpoint => {
@@ -58,47 +69,69 @@ export default class Visualise extends React.Component {
     console.log('pickImages')
     /* randomly pick imageSet from this.state.liveList */
     let randomSet = Math.floor(Math.random() * this.state.liveList.length);
-    this.setState({
-      imageSet: this.state.liveList[randomSet].urls_title
-    });
+    return new Promise(() => {
+      this.setState({
+        imageSet: this.state.liveList[randomSet].urls_title
+      }, this.setState({
+        imageIndex: 0
+      }))
+    })
   };
   changeImage() {
     /* every Xsecs step through the this.state.imageSet until end... then pickImages() */
     console.log('changing imageSet url');
-    if (this.state.imageIndex < this.state.imageSet.length-1) {
+    if (this.state.imageIndex < this.state.imageSet.length - 1) {
       this.setState({
         imageIndex: this.state.imageIndex + 1
       })
     } else {
       this.pickImages();
-      this.setState({
-        imageIndex: 0
-      })
     }
   };
+  startTimer(id) {
+    id = setInterval(() => this.changeImage(), 2000);
+    //console.log(id);
+    return {
+      id: id
+    };
+  }
+  endTimer(id) {
+    //console.log(id);
+    clearInterval(id);
+  }
   componentDidMount() {
-    /* load  story from database into state */
-    //setInterval(this.changeImage(), 2000);
-    this.apiGet(this.state.apiHello)
-      .then(res => {
-        console.log(res.liveList);
-        this.setState({
-          liveList: res.liveList
-        });
-      }).then(() => {
-        this.pickImages();
-      }).then(() => {
-        setInterval(this.changeImage(), 2000)
-      }).catch(err => console.log(err));
+    this._isMounted = true;
     /* open sse listener */
     this.eventSource.addEventListener('activelistChange', (e) => {
       console.log('Backend changes triggered a refresh of the activelist');
       this.refreshList();
     });
-    // Catches errors
+    /* Catches errors */
     this.eventSource.onerror = (e) => {
       console.log("--- SSE EVENTSOURCE ERROR: ", e);
     };
+    /* connect to API */
+    if (this._isMounted) {
+      this.apiGet(this.state.apiHello)
+        .then(res => {
+          console.log(res.liveList);
+          /* load liveList from database into state */
+          this.setState({
+            liveList: res.liveList
+          });
+        }).then(() => {
+          /* randomly pick imageSet from liveList */
+          this.pickImages();
+        }).then(() => {
+          /* start the changeImage-timer */
+          timerId = this.startTimer().id;
+        }).catch(err => console.log(err));
+    }
+  }
+  componentWillUnmount() {
+    /* cancel the changeImage-timer */
+    this.endTimer(timerId);
+    this._isMounted = false;
   }
   render() {
     console.log(this.state)
@@ -110,7 +143,7 @@ export default class Visualise extends React.Component {
         />
         <hr/>
         <div id="imagePlayer" style={this.state.image_frame} ref={container => {this.innerContainer=container}}>
-          <img id="images" alt="" src={this.state.imageSet[this.state.imageIndex]} style={this.state.image}/>
+          <CrossfadeImage style={this.state.style} alt="" src={this.state.imageSet[this.state.imageIndex]}/>
         </div>
       </div>
     );
