59c25bcaa5101e1e307bb5826146b0db31d4f565
Léon McCarthy <leonmccarthy@Leons-MacBook-Pro.local>
2020-02-21 09:26:19 +0000

    Switch from CRA to react-app-rewired

---
+++ client/config-overrides.js
@@ -0,0 +1,18 @@
+//const { injectBabelPlugin } = require("react-app-rewired");
+
+/*
+module.exports = function override(config, env) {
+  //do stuff with the webpack config...
+  devServer: {
+    historyApiFallback: true
+  }
+  return config;
+}
+*/
+
+const {override,addBabelPlugins} = require("customize-cra");
+module.exports = override(
+  ...addBabelPlugins(
+    "babel-plugin-transform-remove-console"
+  )
+);
