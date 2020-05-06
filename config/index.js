18bee24515bc3fc0718db13546e2a0e30ddffe39
Léon McCarthy <leonmccarthy@Leons-MacBook-Pro.local>
2020-05-06 15:45:40 +0100

    Snyk fixes applied

--- config/index.js
+++ config/index.js
@@ -1,16 +1,24 @@
-const _ = require('lodash');
+// const _ = require('lodash');
 
-// const dotenv = require('dotenv');
-
-// dotenv.config();
 // load the ENVIRONMENT variables
 require('dotenv').config();
+
+/* module debugging */
+const debug = require('debug')('config');
 
 const dbConfig = require('./dbConfig');
 const networkConfig = require('./networkConfig');
 const secretConfig = require('./secretConfig');
 const setupConfig = require('./setupConfig');
 
-const finalConfig = _.merge(dbConfig, networkConfig, secretConfig, setupConfig);
+// const finalConfig = _.merge(dbConfig, networkConfig, secretConfig, setupConfig);
+const finalConfig = {
+  ...dbConfig,
+  ...networkConfig,
+  ...secretConfig,
+  ...setupConfig,
+};
+
+debug(finalConfig);
 
 global.config = finalConfig;
