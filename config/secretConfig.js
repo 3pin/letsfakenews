76bf7630bde35eef99532bd3a2ab57fe785e112d
Léon McCarthy <leonmccarthy@Leons-MacBook-Pro.local>
2020-05-19 22:19:41 +0100

    Change to config files

--- config/secretConfig.js
+++ config/secretConfig.js
@@ -1,7 +1,5 @@
 module.exports = {
   customSearchApikey: process.env.CUSTOM_SEARCH_APIKEY,
   customSearchEngineId: process.env.CUSTOM_SEARCH_ENGINE_ID,
-  username: process.env.USERNAME,
-  password: process.env.PASSWORD,
   secret: process.env.SECRET,
 };
