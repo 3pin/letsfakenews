ee121a0a98b1ab08384d94d1f6a55438a9ed8aaf
Léon McCarthy <leonmccarthy@Leons-MacBook-Pro.local>
2020-05-22 23:12:38 +0100

    Changed the description on /write/landing

--- client/src/app/pages/write/landing.js
+++ client/src/app/pages/write/landing.js
@@ -7,7 +7,7 @@ import FrameButton from '../../components/frameButton';
 const Landing = () => (
   <div>
     <section>
-      <FrameBanner title="Create fake-news" desc="You're a journalist with nothing to report, so trick the news room with fake news. To ensure they publich it - use correct spelling, grammar & punctuation." />
+      <FrameBanner title="Create fake-news" desc="Pretend you're a journalist with nothing to report, tricking the news-room with a fake story. To ensure it's published use correct spelling, grammar & punctuation." />
       <hr />
       <FrameButton linkto="/write/story" buttonLabel="Start" />
       <hr />
