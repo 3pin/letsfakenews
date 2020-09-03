f372bb0c5a5956691c32c6943589d9a96303e07d
Léon McCarthy <leonmccarthy@Leons-MacBook-Pro.local>
2020-09-03 15:45:29 +0100

    package.json: specificed node version via engines

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -1,15 +1,3 @@
-// const { injectBabelPlugin } = require("react-app-rewired");
-
-/*
-module.exports = function override(config, env) {
-  //do stuff with the webpack config...
-  devServer: {
-    historyApiFallback: true
-  }
-  return config;
-}
-*/
-
 const { override, addBabelPlugins } = require('customize-cra');
 
 module.exports = override(
